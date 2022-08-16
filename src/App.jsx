cfb9cb48f190516270c36411e4a3a040127e71e5
Manav Misra <manavm@visionwebsoft.com>
2022-08-16 11:54:19 -0500

    Render Header with Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import "./App.css";
-import Header from "./components/Header";
+import Form from "./components/Form/Form";
+import Header from "./components/Header/Header";
 
+// TODO: Create a Form component that we will use to update the name upon submission
 function App() {
-  const myName = "World";
+  const [name, setName] = useState("Mark");
 
-  return <Header name={myName} />;
+  return (
+    <>
+      <Header name={name} />
+      <Form />
+    </>
+  );
 }
 
 export default App;
