0784edb4210affc20406cb0f8aab89036f473441
Manav Misra <manavm@visionwebsoft.com>
2022-08-17 09:44:57 -0500

    Show conditional rendering
    Includes pasted solution.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
 import "./App.css";
 import Form from "./components/Form/Form";
+import Profile from "./components/Profile/Profile";
+
+const imgUrl = "https://i.imgur.com/yXOvdOSs.jpg";
 
 function App() {
-  const [name, setName] = useState("Mark");
+  const [name, setName] = useState("");
 
   return (
     <>
       <h1 className="my-8 text-center text-3xl">Hello {name}</h1>
+
+      {/* Conditional Rendering */}
+      {name && <Profile name={name} imgUrl={imgUrl} />}
       <Form setName={setName} />
     </>
   );
